c3e7539cfb72eb6ccf5f1c8c39d216375e3289ad
Wicca <alphayojoseph1738@gmail.com>
2023-01-24 08:32:03 +0800

    WiccaToken code sample

---
+++ brownie/myNFT/contracts/myToken.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "OpenZeppelin/openzeppelin-contracts@4.0.0/contracts/token/ERC20/ERC20.sol";
+
+contract WiccaToken is ERC20{
+    constructor(uint256 initialSupply) ERC20("Wicca", "WIC"){
+        _mint(msg.sender, initialSupply);
+    }
+}
---
+++ brownie/myNFT/scripts/polyg0n_settings.bash
@@ -0,0 +1,2 @@
+#!bin/bash
+brownie networks add [environment] [id] host=[host] [KEY=VALUE, ...]
